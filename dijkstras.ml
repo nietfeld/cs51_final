b635de22dcf97182135f95cbbd5bbfbf9870f77f
Anastasiya Borys <anastasiyaborys@college.harvard.edu>
2013-04-10 12:46:33 -0400

    updated

---
+++ dijkstras.ml
@@ -0,0 +1,31 @@
+(* this is going to be very similar *)
+
+let sort (m : (module PRIOQUEUE with type elt=IntCompare.t)) (lst : int list) =
+  let module P = (val (m) : PRIOQUEUE with type elt = IntCompare.t) in
+
+  let rec extractor pq lst =
+    if P.is_empty pq then lst else
+    let (x, pq') = P.take pq in
+    extractor pq' (x::lst) in
+  let pq = reduce (fun x y -> P.add x y) P.empty lst in
+  List.rev (extractor pq [])
+
+
+(* Hurray!! Now, we can pass in the modules into sort and get out
+ * different sorts!! *)
+
+(* Sorting with a priority queue with an underlying heap
+ * implementation is equivalent to heap sort! *)
+let heapsort = sort heap_module
+
+(* Sorting with a priority queue with your underlying tree
+ * implementation is *almost* equivalent to treesort;
+ * a real treesort relies on self-balancing binary search trees *)
+
+let treesort = sort tree_module
+
+(* Sorting with a priority queue with an underlying unordered list
+ * implementation is equivalent to heap sort! If your implementation of
+ * ListQueue used ordered ilsts, then this is really insertion sort *)
+let selectionsort = sort list_module
+
