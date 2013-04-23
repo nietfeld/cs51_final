92d64014beb21cf36abe2f121a4e4704e56d7c17
John Harvard <jharvard@appliance.localdomain>
2013-04-23 18:57:49 -0400

    new dijkstras code

---
+++ new_dij.ml
@@ -0,0 +1,209 @@
+
+(* this is where we need to replace with our prioq *)
+(* this is likely going to be deletemin from the prioq *)
+(* also here we will insert the edges that are connected to the 
+current node ?????????????????????????????????????????/ *)
+
+(************************ CHANGE TO DELETE-MIN FROM PRIOQUEUE **************)
+(*
+let first_not_treated pq = 
+  try 
+    for i=0 to cs.nn-1 do 
+      if not cs.already_treated.(i) then raise (Found i)
+    done;
+    (*raise Not_found ;*) (* ask about this *)(* gives a warning - why *)
+    0
+  with QueueEmpty -> failwith "done" ;;
+*)
+
+(* the thing we're popping off prioq *)
+(* will make tuple more general type later - now node ID in graph and tentative distance from A *) 
+let delete_min (pq : queue) : ((int*float) * queue) = 
+  try 
+    let new_node = pq.take
+    (* but we don't want to fail just end *) 
+    (* try display_state from djikstras.ml *) 
+  with QueueEmpty -> Printf.printf "done" ;;
+
+(*
+(**************** CHANGE TO UPDATING VALUE IN PRIOQUEU ****************)
+let least_not_treated p cs =
+  let ni = ref p  
+  and nd = ref cs.distances.(p) in 
+  for i=p+1 to cs.nn-1 do 
+    if not cs.already_treated.(i) then 
+      if less_cost cs.distances.(i)  !nd then 
+        ( nd := cs.distances.(i);
+          ni := i )
+  done;
+  !ni,!nd;;
+
+ exception No_way;; *)
+
+
+(* loop through neighbors list (assume for now int * float) *) 
+ let rec update_queue (pq : queue) (curr_node: int*float) (neighbor_list : (int * float) list) : queue = 
+   let (node_id, distance_from_start) = curr_node in 
+   match neighbor_list with 
+   | [] -> pq 
+   | (n,e)::tl -> 
+     (* assume look up k is a function into index k of the array *) 
+     (match Array.get n dist with
+     | Nan -> 
+       (* look up id n in prioq- is there a way to only do this way? *) 
+       let (k, tent_dist) = pq.lookup n in
+       let new_dist = e + distance_to in 
+       if new_dist < tent_dist then 
+	 (* update priority q *)
+	 (let new_pq = pq.update_key k new_dist in 
+	 (* we have a new shorter path to k through n *) 
+	  Array.set prev k n;
+	  update_queue new_pq curr_node tl)
+       (* don't update, do next neighbor *) 
+       else update_queue pq curr_node tl
+     | _ -> update_queue pq curr_node tl) 
+      
+ (* ???????? *) 
+ let one_round (pq: queue) (last_node: (my_graph: node graph) : queue = 
+   let (curr_node, distance_to) = pq.take in 
+   let neighbor_list = neighbors n my_graph in 
+   (* for more general elements, use node int compare to get string to an int *)
+   (* update the distance array *) 
+   Array.set dist curr_node distance_to;
+   update_queue pq (curr_node, distance_to) neighbor_list
+
+
+(*
+ let one_round cs g = 
+   (*********** DELETE MIN, update prev and dist array  *****************)
+   let p = first_not_treated cs  in 
+   (*********** get neighbors from graph ***********)
+   let np,nc = least_not_treated p cs in
+   if not(a_cost nc  ) then raise No_way 
+   else 
+     begin
+       cs.already_treated.(np) <- true;
+       (* for each neighbor *) 
+       for i = 0 to cs.nn -1 do
+         if not cs.already_treated.(i) then 
+           if a_cost g.m.(np).(i) then
+	     (* update priority queue values *) 
+             let ic = add_cost cs.distances.(np)  g.m.(np).(i) in 
+             if less_cost ic cs.distances.(i)   
+	     then (cs.paths.(i) <- np;
+               cs.distances.(i) <- ic) 
+       done; cs
+     end;;
+
+ let dij =
+   let dest = Array.make n in 
+   one_round *) 
+
+(* Need to finish implementing prioq *) 
+module IntListQueue = (ListQueue(NodeCompare) :
+                        PRIOQUEUE with type elt = NodeCompare.t)
+module IntHeapQueue = (BinaryHeap(NodeCompare) :
+                        PRIOQUEUE with type elt = NodeCompare.t)
+let list_module = (module IntListQueue : PRIOQUEUE with type elt = NodeCompare.t)
+let heap_module = (module IntHeapQueue : PRIOQUEUE with type elt = NodeCompare.t)
+
+let initialize_queue (n: int) start: queue = 
+  let rec add_nodes (pq: queue) (to_add: int) : queue = 
+    if to_add = 0 then pq 
+    else add_nodes (P.add (to_add, Nan) pq) (to_add - 1)
+  (* for starting node, we want the id and dist 0. *) 
+  in add_nodes (P.add (start, 0) P.empty) (n-1)
+ (* add the starting node with dist 0*) 
+
+let dij start g (pq : (module PRIOQUEUE with type elt=NodeCompare.t)) = 
+  let module P = (val (pq) : PRIOQUEUE with type elt = NodeCompare.t) in
+  (*let prioq = P.empty in*)
+  (* make a prioq with all nodes in graph set to infinity *) 
+  if g.has_node start g then 
+    (* MAKE FUNCTION *) 
+    let graph_size = g.number_nodes in
+    (* make distance and prev arrays *)
+    (* write way to have them all be in scope *) 
+    let dist = Array.make graph_size Nan in 
+    let prev = Array.make graph_size -1 in 
+    let prioq = initialize_queue graph_size start
+    (* make priority queue *) 
+    begin
+      let i = index s g in 
+      let cs = { paths = Array.create g.ind (-1) ;
+                 already_treated = Array.create g.ind false;
+                 distances = Array.create g.ind Nan;
+                 nn = g.ind;
+                 source = i}  in
+      cs.already_treated.(i) <- true; 
+      (* make this recursive *) 
+      for j=0 to g.ind-1 do 
+        let c = g.m.(i).(j) in 
+        cs.distances.(j) <- c;
+        if a_cost c then cs.paths.(j) <- i 
+      done;
+      try
+        for k = 0 to cs.nn-2 do 
+          ignore(one_round cs g) 
+        done;
+        cs
+      with No_way -> cs
+    end
+  else failwith "dij: node unknown";;
+
+
+
+(*
+let dij s g (pq : (module PRIOQUEUE with type elt=NodeCompare.t)) = 
+  let module P = (val (pq) : PRIOQUEUE with type elt = NodeCompare.t) in
+  (*let prioq = P.empty in*)
+  (* make a prioq with all nodes in graph set to infinity *) 
+  if belongs_to s g then 
+    begin
+      let i = index s g in 
+      let cs = { paths = Array.create g.ind (-1) ;
+                 already_treated = Array.create g.ind false;
+                 distances = Array.create g.ind Nan;
+                 nn = g.ind;
+                 source = i}  in
+      cs.already_treated.(i) <- true; 
+      (* make this recursive *) 
+      for j=0 to g.ind-1 do 
+        let c = g.m.(i).(j) in 
+        cs.distances.(j) <- c;
+        if a_cost c then cs.paths.(j) <- i 
+      done;
+      try
+        for k = 0 to cs.nn-2 do 
+          ignore(one_round cs g) 
+        done;
+        cs
+      with No_way -> cs
+    end
+  else failwith "dij: node unknown";;
+*)
+'
+(* displaying the results here *)
+(* Will update for better display *) 
+ let display_state f (g,st)  dest = 
+   if belongs_to dest g then 
+      let d = index dest g in
+        let rec aux is = 
+           if is = st.source then Printf.printf "%a"  f g.nodes.(is)
+           else (
+             let old = st.paths.(is) in 
+             aux old;
+             Printf.printf " -> (%4.1f) %a" (float_of_cost g.m.(old).(is))
+                                          f g.nodes.(is)  
+           )
+        in 
+          if not(a_cost st.distances.(d)) then Printf.printf "no way\n"
+          else (
+            aux d;
+            Printf.printf " = %4.1f\n" (float_of_cost st.distances.(d)));;
+
+let g = test_aho ();;
+let r = dij "A" g list_module;;
+
+display_state (fun x y -> Printf.printf "%s!" y) (a,r) "E";; 
+(* this is what should return *)
