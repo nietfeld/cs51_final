3788333642b24541e4d54a84203873ade159774d
Melanie Justiniano <melaniejustiniano@college.harvard.edu>
2013-04-23 20:03:54 -0400

    Graphs with IDs

--- graphs.ml
+++ graphs.ml
@@ -25,13 +25,13 @@ sig
 
   (* Adds the nodes if they aren't already present. *)
   (* and the weight of that edge *)
-  val add_edge : graph -> node -> node -> int -> graph
+  val add_edge : graph -> node -> node -> float -> graph
     
   (* Return None if node isn't in the graph *)
-  val neighbors : graph -> node -> (node * int) list option
+  val neighbors : graph -> node -> (node * float) list option
 
   (* Return None if node isn't in the graph *)
-  val outgoing_edges : graph -> node -> (node * int * node) list option
+  val outgoing_edges : graph -> node -> (node * float * node) list option
     
   val has_node : graph -> node -> bool
 
@@ -56,15 +56,15 @@ struct
 (* not sure which one is correct here because of the merge *)
   module NeighborSet = Myset.Make(
      struct
-        type t = N.node * int
+        type t = N.node * float
         let compare (n1, w1) (n2, w2) = N.compare n1 n2
-        let string_of_t (n, w) = N.string_of_node n ^ ", " ^ string_of_int w
-        let gen () = (N.gen (), 1)
-        let gen_random () = (N.gen (), 1)
-        let gen_gt x () = (N.gen (), 2)
-        let gen_lt x () = (N.gen (), 0)
+        let string_of_t (n, w) = N.string_of_node n ^ ", " ^ string_of_float w
+        let gen () = (N.gen (), 1.)
+        let gen_random () = (N.gen (), 1.)
+        let gen_gt x () = (N.gen (), 2.)
+        let gen_lt x () = (N.gen (), 0.)
 (*
-        type t = node * int
+        type t = node * float
         let compare = N.compare
         let string_of_t = N.string_of_node
         let gen = N.gen
@@ -140,12 +140,12 @@ struct
        num_nodes = g'.num_nodes;
        index_to_node_map = g'.index_to_node_map}
 
-  let neighbors g n : (node * int) list option = 
+  let neighbors g n : (node * float) list option = 
     match EdgeDict.lookup g.edges n with
       | None -> None
       | Some s -> Some (NeighborSet.fold (fun neigh r -> neigh :: r) [] s)
           
-  let outgoing_edges g src : (node * int * node) list option = 
+  let outgoing_edges g src : (node * float * node) list option = 
     match EdgeDict.lookup g.edges src with
       | None -> None
       | Some s -> Some (NeighborSet.fold (fun (dst, wt) r -> 
@@ -176,17 +176,16 @@ struct
                   let string_of_node = fun x -> x
                   let gen () = ""
                 end))
-  let from_edges (es: (string *  int * string) list) : graph =
+  let from_edges (es: (string *  float * string) list) : graph =
     List.fold_left (fun g (src, wt, dst) -> add_edge g src dst wt) empty es
 end
-
 
 module TestGraph = 
 struct 
   module G = NamedGraph
 
-  let g = G.add_edge G.empty "a" "b" 3;;
-  let g2 = G.add_edge g "a" "c" 4;;
+  let g = G.add_edge G.empty "a" "b" 3.;;
+  let g2 = G.add_edge g "a" "c" 4.;;
 
   let deopt_len lo =
     match lo with
@@ -228,5 +227,53 @@ struct
     assert (G.neighbors g2 "d" = None) ;
 
     assert (let t = deopt_lst (G.neighbors g2 "a") in
-              t = ["b";"c"] or t = ["c";"b"]) )
+              t = [("b", 3.);("c",4.)] or t = [("c",4.);("b",3.)]) )
+
+  let g3 = G.from_edges [("a",2.,"b");("a",3.,"c");("b",4.,"d");
+		       ("d",5.,"e");("d",6.,"f");("h",8.,"e");
+		       ("g",7.,"f");("h",9.,"g")] ;;
+
+  assert(let a = deopt_lst (G.neighbors g3 "a") in 
+	 a = [("b",2.);("c",3.)] or a = [("c",3.);("b",2.)]) ;
+  assert(deopt_lst (G.neighbors g3 "b") = [("d",4.)]) ;
+  assert(deopt_lst (G.neighbors g3 "c") = []) ;
+  assert(let d = deopt_lst (G.neighbors g3 "d") in 
+	 d = [("e",5.);("f",6.)] or d = [("f",6.);("e",5.)]) ;
+  assert(deopt_lst (G.neighbors g3 "e") = []) ;
+  assert(deopt_lst (G.neighbors g3 "f") = []) ;
+  assert(deopt_lst (G.neighbors g3 "g") = [("f",7.)]) ;
+  assert(let h = deopt_lst (G.neighbors g3 "h") in 
+	 h = [("e",8.);("g",9.)] or h = [("g",9.);("e",8.)]) 
+
+  let tester oelist : unit =
+    match oelist with
+    | None -> print_string ""
+    | Some s -> print_string (List.fold_right (fun (dst, wt, src) y -> 
+      dst ^ " & " ^ string_of_float wt ^ " & " ^ src ^ " ; " ^ y) s "")
+;;
+
+(* Tested by printing out all graph edges and checking manually.
+
+assert(tester (G.outgoing_edges g3 "a") = ()) ;
+assert(tester (G.outgoing_edges g3 "b") = ()) ;
+assert(tester (G.outgoing_edges g3 "c") = ()) ;
+assert(tester (G.outgoing_edges g3 "d") = ()) ;
+assert(tester (G.outgoing_edges g3 "e") = ()) ;
+assert(tester (G.outgoing_edges g3 "f") = ()) ;
+assert(tester (G.outgoing_edges g3 "g") = ()) ;
+assert(tester (G.outgoing_edges g3 "h") = ())
+*)
 end
+
+module IdGraph = 
+struct
+  include(Graph(struct
+                  type node = {name : string; id : int}
+                  let compare = fun a b -> Order.int_compare a.id b.id
+                  let string_of_node = fun a -> a.name
+                  let gen () = {name = "A"; id = 1}
+                end))
+  let from_edges (es: (node *  float * node) list) : graph =
+    List.fold_left (fun g (src, wt, dst) -> add_edge g src dst wt) empty es
+end
+
